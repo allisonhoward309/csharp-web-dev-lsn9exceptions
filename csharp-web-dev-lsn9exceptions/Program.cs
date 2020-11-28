2960cfcfd23bcf6b67a9f6000d2ae92199ca8d6e
Allison Howard <allisonhoward309@gmail.com>
2020-11-27 19:59:04 -0600

    final

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -7,27 +7,71 @@ namespace csharp_web_dev_lsn9exceptions
     {
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0.0)
+            {
+                throw new ArgumentOutOfRangeException("Divide by 0 error.");
+            }
+            else
+            {
+                return x / y;
+            }
+
         }
+
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            string substg = fileName.Substring(fileName.Length - 3);
+            if (fileName == null || fileName == "")
+            {
+                throw new ArgumentNullException("No file was submitted.");
+            }
+            else if ( substg == ".cs")
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
 
         static void Main(string[] args)
         {
-            // Test out your Divide() function here!
+            double x = 9.9;
+            double y = 0.0;
 
-            // Test out your CheckFileExtension() function here!
+            try
+            {
+                Divide(x, y);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // Help out the teacher!
             Dictionary<string, string> students = new Dictionary<string, string>();
             students.Add("Carl", "Program.cs");
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+            foreach (KeyValuePair<string, string> student in students)
+            {
+                try
+                {
+                    Console.WriteLine(student.Key);
+                    CheckFileExtension(student.Value);
+                }
+                catch (ArgumentNullException n)
+                {
+                    Console.WriteLine(n.Message);
+                }
+            }
         }
     }
 }
+
+    
